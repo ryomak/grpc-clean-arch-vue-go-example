b5002ef50aaa4ea35ffafd91caa2919596a7a124
ryomak <ryooomaaa0413@outlook.com>
2020-02-22 16:14:55 +0900

    fix mod

--- protobuild.sh
+++
@@ -1,2 +0,0 @@
-protoc -I ./../proto user.proto --js_out=import_style=commonjs:./src/assets --grpc-web_out=import_style=commonjs,mode=grpcwebtext:./src/assets
-protoc -I ./../proto room.proto --js_out=import_style=commonjs:./src/assets --grpc-web_out=import_style=commonjs,mode=grpcwebtext:./src/assets
